f4369713a8480ac14b06c8303533562af7ab7ccf
Sebastián Ortega <sortega@refeed.org>
2023-06-24 18:07:01 +0200

    rust: day 1, part 1

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,26 @@
+use std::fs::File;
+use std::io;
+use std::io::BufRead;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new("inputs/day01.txt");
+    let file = File::open(path).expect("cannot open file");
+    let reader = io::BufReader::new(file);
+
+    let mut max_elf_calories = 0;
+    let mut elf_calories = 0;
+    for line in reader.lines() {
+        let line = line.expect("cannot read line");
+        if line.trim().is_empty() {
+            // Next elf
+            elf_calories = 0;
+        } else {
+            let item_calories: i32 = line.trim().parse().expect("not a number");
+            elf_calories += item_calories;
+            max_elf_calories = max_elf_calories.max(elf_calories);
+        }
+    }
+
+    println!("{max_elf_calories}");
 }
