c9a6c2a7b6b238329e1798417a54477df812b1a2
Sebastián Ortega <sortega@refeed.org>
2023-07-31 18:55:42 +0200

    Day 1, part 2

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -8,19 +8,24 @@ fn main() {
     let file = File::open(path).expect("cannot open file");
     let reader = io::BufReader::new(file);
 
-    let mut max_elf_calories = 0;
-    let mut elf_calories = 0;
+    let mut elves: Vec<i32> = Vec::new();
+    let mut current_elf = 0;
     for line in reader.lines() {
         let line = line.expect("cannot read line");
         if line.trim().is_empty() {
-            // Next elf
-            elf_calories = 0;
+            elves.push(current_elf);
+            current_elf = 0;
         } else {
             let item_calories: i32 = line.trim().parse().expect("not a number");
-            elf_calories += item_calories;
-            max_elf_calories = max_elf_calories.max(elf_calories);
+            current_elf += item_calories;
         }
     }
 
-    println!("{max_elf_calories}");
+    elves.sort();
+    elves.reverse();
+    elves.truncate(3);
+
+    let part1 = elves.first().unwrap();
+    let part2: i32 = elves.iter().sum();
+    println!("part 1: {part1}\npart 2: {part2}");
 }
