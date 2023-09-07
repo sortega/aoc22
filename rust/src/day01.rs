655171535293763a2aa0e001d9a08f5201531f1f
Sebastián Ortega <sortega@refeed.org>
2023-09-07 13:00:45 +0200

    Use iterators and structs in day 1

--- rust/src/day01.rs
+++ rust/src/day01.rs
@@ -1,31 +1,34 @@
-use std::fs::File;
-use std::io;
-use std::io::BufRead;
-use std::path::Path;
+use crate::common::slurp_input;
+
+struct Inventory {
+    item_calories: Vec<i32>,
+}
+
+impl Inventory {
+    fn calories(&self) -> i32 {
+        self.item_calories.iter().sum()
+    }
+}
+
+type Input = Vec<Inventory>;
 
 pub fn solve() {
-    let path = Path::new("inputs/day01.txt");
-    let file = File::open(path).expect("cannot open file");
-    let reader = io::BufReader::new(file);
+    let maybe_calories: Vec<Option<i32>> = slurp_input(1)
+        .iter()
+        .map(|line| line.trim().parse().ok())
+        .into_iter()
+        .collect();
+    let mut input: Input = maybe_calories
+        .split(|option| option.is_none())
+        .map(|items| Inventory {
+            item_calories: items.iter().filter_map(|r| *r).collect(),
+        })
+        .collect();
 
-    let mut elves: Vec<i32> = Vec::new();
-    let mut current_elf = 0;
-    for line in reader.lines() {
-        let line = line.expect("cannot read line");
-        if line.trim().is_empty() {
-            elves.push(current_elf);
-            current_elf = 0;
-        } else {
-            let item_calories: i32 = line.trim().parse().expect("not a number");
-            current_elf += item_calories;
-        }
-    }
+    input.sort_by_cached_key(|inventory| -inventory.calories());
+    input.truncate(3);
 
-    elves.sort();
-    elves.reverse();
-    elves.truncate(3);
-
-    let part1 = elves.first().unwrap();
-    let part2: i32 = elves.iter().sum();
+    let part1 = input.first().unwrap().calories();
+    let part2: i32 = input.iter().map(|inventory| inventory.calories()).sum();
     println!("DAY 01\n======\npart 1: {part1}\npart 2: {part2}\n");
 }
